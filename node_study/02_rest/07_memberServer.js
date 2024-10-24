6efc82e360cd177fa21ac5d5b81048de2769d957
nemo <jx007s@gmail.com>
2024-10-24 14:31:07 +0900

    10_24_member

--- node_study/02_rest/07_memberServer.js
+++ node_study/02_rest/07_memberServer.js
@@ -10,6 +10,26 @@ let mems={
     3:{id:3,pname:'한가인',age:47}
 }
 
+/// req.on을 동기식으로 처리하는 함수
+function reqParam(reqqq){
+
+    return new Promise((resolve, reject)=>{
+        let res = ''
+        reqqq.on('data',ddd=>{
+            res += ddd.toString()  //요청 query 받기
+        })
+        reqqq.on('end',()=>{
+            //param 내보내기
+            resolve(decodeURIComponent(res) )
+        })
+        reqqq.on('error',err=>{
+            reject(err)  //에러발생시 reject
+        })
+    })
+}
+
+
+
 http.createServer(async(req,res)=>{
     const myUrl = url.parse(req.url, true)
 
@@ -21,9 +41,8 @@ http.createServer(async(req,res)=>{
         console.log(memsStr)
         ttt = ttt.toString().replaceAll("'{{mems}}'", memsStr)
 
-    }
 
-    if(myUrl.pathname == '/detail'){
+    }else if(myUrl.pathname == '/detail'){
         ttt = await fs.readFile('07_memberDetail.html')
 
         let nowId = myUrl.query.id
@@ -33,6 +52,37 @@ http.createServer(async(req,res)=>{
         console.log(memsStr)
         ttt = ttt.toString().replaceAll("'{{mem}}'", memsStr)
 
+    }else if(myUrl.pathname == '/join'){
+        if(req.method == 'GET'){
+            ttt = await fs.readFile('07_memberJoinForm.html')
+        }
+        if(req.method == 'POST'){
+
+            /*
+            req.on('data', ddd=>{
+
+            })*/
+
+
+            // "pname=운마니&age=23"   -> URLSearchParams { 'pname' => '운마니', 'age' => '23' }  
+            let param = new URLSearchParams(await reqParam(req))
+            //console.log(param)
+            let newMember = {
+                'pname':param.get('pname'),
+                'age':param.get('age'),
+                'id':maxNum
+            }
+            //console.log(newMember)
+            mems[maxNum] = newMember
+            maxNum++
+
+            let msg = '가입되었습니다.'
+            let dst = '/'
+            ttt = await fs.readFile('07_memberAlert.html')
+            ttt = ttt.toString()
+            .replaceAll('{{msg}}',msg)
+            .replaceAll('{{dst}}',dst)
+        }
     }
 
     
