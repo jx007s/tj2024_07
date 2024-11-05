51e5dc52b217c145cc03759deb73b924eb54ea6f
nemo <jx007s@gmail.com>
2024-11-05 13:25:05 +0900

    11_05_react

---
+++ node_study/05_react/first_app/src/App.js
@@ -0,0 +1,13 @@
+import MyComp1 from './comp1'
+
+function App() {
+  return (
+      <div>
+        <h1>안녕하세요</h1>
+        <p>아기상어 뚜루루뚜루</p>
+        <MyComp1/>
+      </div>
+  );
+}
+
+export default App;
---
+++ node_study/05_react/first_app/src/comp1.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function MyComp1(){
+    return <div>난 comp1 이야</div>
+}
+
+export default MyComp1;
